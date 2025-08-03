bfb5e2bf527ac98b7ff6e78f3ff436c141410b95
ααdhí <87893573+Aadhi000@users.noreply.github.com>
2025-08-03 17:27:24 +0530

    Update stealth.js

--- plugins/stealth.js
+++ plugins/stealth.js
@@ -1,1 +1,15 @@
-const stealth = require('./plugins/stealth')(); 
+// Minimal Stealth Plugin for Playwright (local version)
+module.exports = function stealthPlugin() {
+  return {
+    name: 'stealth',
+    async onPageCreated(page) {
+      await page.addInitScript(() => {
+        // Basic stealth tweaks
+        Object.defineProperty(navigator, 'webdriver', { get: () => false });
+        window.chrome = { runtime: {} };
+        Object.defineProperty(navigator, 'languages', { get: () => ['en-US', 'en'] });
+        Object.defineProperty(navigator, 'plugins', { get: () => [1, 2, 3] });
+      });
+    }
+  };
+};
